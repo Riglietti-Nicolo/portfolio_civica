227ba5e00d1e388484d7ce56eca2d90818258b47
Riglietti-Nicolo <nicolo.riglietti@istitutoagnelli.it>
2026-06-22 21:37:04 +0200

    project

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -146,4 +146,17 @@ export const projects = [
     // cover: "/covers/image10.png",
     schoolYear: "4"
   },
+
+  {
+    id: "13",
+    title: "Incontro con Roberto Saviano",
+    description:
+      'Incontro con Roberto Saviano a partire dal libro "L\'amore mio non muore": riflessione sul coraggio civile, la narrativa come strumento di denuncia e il prezzo personale della lotta alla criminalità.',
+    type: "",
+    year: "2026",
+    topic: "Legalità",
+    // url: "/presentazione_amish.pdf",
+    // cover: "/covers/image10.png",
+    schoolYear: "5"
+  },
 ];
